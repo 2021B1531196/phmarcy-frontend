e8639ecf30c4099dd1698a7048a1a77bce016eea
Sanjana <61216746+sanjanarajkumari@users.noreply.github.com>
2025-01-15 11:17:55 +0530

    Update vite.config.ts
    update 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/phmarcy-frontend/', // Add your repository name here
+  base: '/phmarcy-frontendclone/',  // Ensure this matches your GitHub repository name, Add your repository name here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
